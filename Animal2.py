9d544c2b53cdf92bc30b7c113e87f2c18f01c474
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-13 11:37:44 +0200

    Update Animal2.py

--- Animal2.py
+++ Animal2.py
@@ -1,42 +1,17 @@
-
 class Animal:
-    def __init__(self, name, sounds,eat):
+    def __init__(self, name, sounds):
         self.name = name
         self.sounds = sounds
         
-class Dog(Animal):
-    def DogName(self):
-        return('Rax')     
+
+    def food(self):
+        print("{0} eats".format(self.name))
+
     def sound(self):
-        return("barks") 
-    def eats(self):
-        return("eat")
+        print("{0} barks".format(self.sounds))
+        
+dog = Animal("Rax","Dog")
+cat =Animal("Stormy","cat")
 
-class Cat(Animal):
-    def CatName(self):
-        return('stormy') 
-    def sound(self):
-        return("meow")  
-    def eats(self):
-        return("eat")
-
-d = Dog(' ',' ',' ')
-c = Cat(' ',' ',' ')
-print(d.DogName() + " " + d.sound()+ " " + " and" + " " + d.eats())
-print(c.CatName() + " " + c.sound()+ " " + "and" +  " " +c.eats())
-
-class Home:
-    def __init__(self):
-        self.o1 = Dog('','','')
-        self.o2 = Cat('','','')
-
-    def adoptPet(self):
-        print('cat','dog')
-
-    def MakeAllSounds(self):
-        return self.o1.bark()
-
-Rax = Home('bark',' meow','eat')
-Rax.bark()        
-
-  
+dog.food()
+dog.sound()
