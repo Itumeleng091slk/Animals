d1ac19ad03291e7b113194e171b2a03e2930e615
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-13 11:56:53 +0200

    Create Cat.py

---
+++ Cat.py
@@ -0,0 +1,15 @@
+from test import Animal
+
+class Cat(Animal):
+   def __init__(self, name, sounds):
+        super().__init__(name,sounds)
+
+   def food(self):
+        print(self.name + " eats")  
+
+   def sound(self):
+        print(self.sounds,"meows")
+
+cat_1 = Cat("Stormy","Cat")
+cat_1.food()
+cat_1.sound()
